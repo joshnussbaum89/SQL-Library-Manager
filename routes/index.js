8c45e71e6e9f9f529521381c4bfd0e5d8932d189
Josh Nussbaum <Joshnussbaum89@gmail.com>
2021-03-02 16:31:58 -0600

    setup and test Book model

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Book = require('../models').Book;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* Handler function to wrap each route. */
+const asyncHandler = (cb) => {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch (error) {
+      // Forward error to the global error handler
+      next(error);
+    }
+  }
+}
+
+// Home
+router.get('/', asyncHandler(async (req, res, next) => {
+  // res.render('index', { title: 'Express' });
+  const books = await Book.findAll();
+}));
 
 module.exports = router;
